73c0490437f5cbf49cb5d57fa9822d95ae8f33c5
Val Milkevich <milkevich@gmail.com>
2013-01-04 20:19:50 +0300

    Helpers for doc

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,38 @@
+module ApplicationHelper
+	def doc nav = false, &block
+		content_tag(:div, content_tag(:ul, capture(&block), :class => nav ? 'nav-header' : ''), :class => "span6")
+	end
+
+	def desc nav = false, &block
+		content_tag(:div, content_tag(:ul, capture(&block), :class => nav ? 'nav-header' : ''), :class => "span6 defs")
+	end
+
+	def row &block
+		content_tag :div, capture(&block), :class => "row-fluid"
+	end
+
+	def doc_row *args, &block
+		if args.size == 1
+			d2 = args[0]
+		elsif args.size == 2
+			if args[1].is_a?(TrueClass) || args[1].is_a?(FalseClass)
+				d2 = args[0]
+				nav = args[1]
+			else
+				d1 = args[0]
+				d2 = args[1]
+			end
+		else
+			d1 = args[0]
+			d2 = args[1]
+			nav = args[2]
+		end
+
+		nav ||= false
+
+		row do
+			[(doc(nav){(block.present? ? capture(&block) : d1)} if d1 || block), desc(nav){d2}].compact.join.html_safe
+		end
+	end
+
+end
