f0ead0f55793959e9a34583a009517f3b9456333
Val Milkevich <milkevich@gmail.com>
2013-01-08 13:32:52 +0300

    deploy sh

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+git st & wait
+git co master & wait
+git push h & wait
+git co worker & wait
+git merge master & wait
+git push hw0 worker:master & git push hw1 worker:master & git push hw2 worker:master & wait
