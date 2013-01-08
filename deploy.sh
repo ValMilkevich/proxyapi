36da6473ebbb17d28ff484cc3595de5f2483fa61
Val Milkevich <milkevich@gmail.com>
2013-01-08 13:37:32 +0300

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 git st & wait
 git co master & wait
+echo 'push to master'
 git push h & wait
 git co worker & wait
 git merge master & wait
+echo 'push to workers'
 git push hw0 worker:master & git push hw1 worker:master & git push hw2 worker:master & wait
+exit
