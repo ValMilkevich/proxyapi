6afd215bc5a318456387c939c106c2bea1bd7d06
Val Milkevich <milkevich@gmail.com>
2013-05-06 18:22:32 +0300

    schedule rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,11 +8,13 @@
 set :output, "log/cron.log"
 #
 
-every '0 * * * *'  { rake "proxies:hidemyass:get" }
-every '10 * * * *' { rake "proxies:incloack:get"}
-every '20 * * * *' { rake "proxies:spys:get" }
-every '30 * * * *' { rake "proxies:dj:bulk_invoke" }
-every '40 * * * *' { rake "proxies:freedailyproxy:get" }
+every 20.minutes do
+  rake "proxies:hidemyass:get"
+  rake "proxies:incloack:get"
+  rake "proxies:spys:get"
+  rake "proxies:dj:bulk_invoke"
+  rake "proxies:freedailyproxy:get"
+end
 
 every 1.day do
   rake "proxies:dj:check"
