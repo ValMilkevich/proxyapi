464332d3b1d4cf4d4c47cce238e557addc1f4562
Val Milkevich <milkevich@gmail.com>
2013-12-25 23:24:12 +0300

    hi

---
+++ app/models/cmd/config.rb
@@ -0,0 +1,16 @@
+class Cmd::Message
+  include Mongoid::Document
+  
+  field :cmd, type: String
+  field :re, type: String
+  field :throttle, type: String
+  field :logfile, type: String
+  
+  
+  # {
+  #   :cmd => "-q -L 3342 #{proxy ? "-x #{proxy.to_s}" : '' }",
+  #   :re => 60 * 10,
+  #   :throttle => 10
+  # }
+  # 
+end
