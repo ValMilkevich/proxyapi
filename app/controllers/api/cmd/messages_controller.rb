364a92b2c7a0b86a0a1a314c0924806948f38983
Val Milkevich <milkevich@gmail.com>
2013-12-25 05:11:53 +0300

    hi

--- app/controllers/api/cmd/messages_controller.rb
+++ app/controllers/api/cmd/messages_controller.rb
@@ -1,5 +1,5 @@
 class Api::Cmd::MessagesController < ApiController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :only => :index
   
   def cmd
     # JSON.load(open("http://proxyapi.info/api/proxies/selector/last_check(#{6.hours.ago.strftime('%Y%m%d%H%M')}-202012010000):order(latency.desc):availability(1,0-0.99):available(true):checks_count(10-10000000).json")).sample
