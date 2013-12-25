f7ae6be6cf7b28e4f347617477bb65b496f9a1b5
Val Milkevich <milkevich@gmail.com>
2013-12-25 04:57:34 +0300

    admin namespace

--- app/controllers/api/cmd/messages_controller.rb
+++ app/controllers/api/cmd/messages_controller.rb
@@ -1,5 +1,6 @@
 class Api::Cmd::MessagesController < ApiController
-	
+  before_filter :authenticate_user!
+  
   def cmd
     # JSON.load(open("http://proxyapi.info/api/proxies/selector/last_check(#{6.hours.ago.strftime('%Y%m%d%H%M')}-202012010000):order(latency.desc):availability(1,0-0.99):available(true):checks_count(10-10000000).json")).sample
     render :json => {:cmd => "-q -L 3342"} #
@@ -11,5 +12,10 @@ class Api::Cmd::MessagesController < ApiController
   
   def index
     render :json => Cmd::Message.all
-  end
+  end  
+
+	protected
+	def authenticate_user!
+		redirect_to :root, :flash => {:error => "Access denied"} if !(current_user && current_user.admin?)
+	end
 end
