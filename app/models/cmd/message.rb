6f620a3a5f8f1840097469ce57469638e9c82257
Val Milkevich <milkevich@gmail.com>
2013-12-26 09:20:08 +0300

    hi

--- app/models/cmd/message.rb
+++ app/models/cmd/message.rb
@@ -5,4 +5,17 @@ class Cmd::Message
   field :body, type: String
   field :name, type: String
   field :proc_status, type: String
+
+  def self.by_hour( from = 1.month.ago)
+    Cmd::Message.where(:created_at.gte => from ).group_by{|m| m.created_at.strftime('%Y-%m-%d %H:00') }
+  end
+  
+  def self.khash_by_hour( from = 1.month.ago)
+    res = {}
+    by_hour(from).each do |date, arr|
+      res[date] = arr.sum{|a| a.scan(/([\d]+\.[\d]+) khash\/s/).flatten.first.to_f} / 6
+    end
+    res
+  end
 end
+
